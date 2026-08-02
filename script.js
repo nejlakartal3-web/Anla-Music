4c622ca5fd380d5b1ed4c06a84368cb3182b2710
nejlakartal3-web <nejlakartal3@gmail.com>
2026-08-02 19:45:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Anla-Music hazır!");
+console.log("Anla-Music hazır!");function createSong() {
+
+  const title = document.getElementById("title").value;
+  const topic = document.getElementById("topic").value;
+  const genre = document.getElementById("genre").value;
+  const mood = document.getElementById("mood").value;
+
+  document.getElementById("result").innerHTML = `
+    <h3>${title}</h3>
+
+    <p><b>Konu:</b> ${topic}</p>
+
+    <p><b>Tür:</b> ${genre}</p>
+
+    <p><b>Duygu:</b> ${mood}</p>
+
+    <br>
+
+    <h4>🎶 Anla AI hazırlanıyor...</h4>
+
+    <p>Yakında bu bilgilerle tamamen kendi yapay zekâmız şarkı oluşturacak.</p>
+  `;
+
+}
